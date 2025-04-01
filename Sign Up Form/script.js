e1ef2cbd9d17d2ebc21d3e0585e7efdded3b7566
Rodrigues <dirs26@gmail.com>
2025-04-01 12:21:10 +0200

    Project done.

--- Sign Up Form/script.js
+++ Sign Up Form/script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
 const signUpBtn = document.querySelector(".green-bg button");
 
-signUpBtn.addEventListener("click", () => {
-  container.classList.toggle("change");
+signUpBtn.addEventListener('click', () => {
+  container.classList.toggle('change');
 });
